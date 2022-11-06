6e927dcc90a19823902c8d0e5261e0a52f50a60b
Aniket Chaurasiya <codwidpp@gmail.com>
2022-11-06 14:15:35 +0530

    api added & response optimization

--- routes/users.ts
+++ routes/users.ts
@@ -1,7 +1,8 @@
-import express, { Router } from 'express';
+import express, { response, Router } from 'express';
 const router:Router = express.Router();
 import { User, Users } from '../models/user';
 import Joi from 'joi';
+import { request } from 'http';
 
 // import { auth } from '../middleware/auth';
 // import nodmailer from 'nodemailer'
@@ -12,33 +13,42 @@ import Joi from 'joi';
 
 const users = new Users("", "");
 
-router.get('/'  , async ( request , response) => {
+router.get('/', async ( request , response) => {
     const findAllUsers = await users.getAllUsers();
+
+    response.setHeader('Content-Type', 'application/json');
+
     if (findAllUsers){
 
         console.log(true)
-        response.send(findAllUsers);
+        // response.send(findAllUsers);
+        response.send(JSON.stringify({ "Message": findAllUsers }));
         return
-
     }
     console.log(false);
-    response.status(404).send("No users found.....!!");
-})
+    // response.status(404).send("No users found.....!!");
+    response.status(404).send(JSON.stringify({ "Error": "No users found.....!!" }));
+});
+
 
 router.get('/:user_id', async( request , response )=> {
 
     const findUser = await users.getUser(request.params.user_id);
     
+    response.setHeader('Content-Type', 'application/json');
+
     if (findUser){
 
         console.log(true)
-        response.json(findUser);
+        // response.json(findUser);
+        response.send(JSON.stringify({ "Message": findUser }));
         return
 
     }
     console.log(false)
-    response.status(404).send("No such user found.....!!");
-})
+    // response.status(404).send("No such user found.....!!");
+    response.status(404).send(JSON.stringify({ "Error": "No such user found.....!!" }));
+});
 
 
 router.post('/', async( request , response ) => {
@@ -51,60 +61,105 @@ router.post('/', async( request , response ) => {
 
     const isMembership:boolean  = await user.isMembership();
 
+    response.setHeader('Content-Type', 'application/json');
+
     if( !isMembership ){
         let isMembershipCreated:boolean  = await user.createMembership();
 
         if( isMembershipCreated ) {
 
             let isSentOtp:boolean = await user.sendOtp()
-            if( isSentOtp ) response.send('New user created successfully. OTP has been sent to your registered mobile number...');
-            else response.status( 400 ).send('Server is down Please contact your IT adminstration.');
+            if( isSentOtp ) response.send(JSON.stringify({ "Message": "New user created successfully. OTP has been sent to your registered mobile number..." }));
+            else response.status( 400 ).send(JSON.stringify({ "Error": "Server is down Please contact your IT adminstration." }));
             return;
         }
     }
     else{
         let isSentOtp:boolean = await user.sendOtp()
-        if( isSentOtp ) response.send('Existing user. OTP has been sent to your registered mobile number...');
-        else response.status( 500 ).send('Server is down Please contact your IT adminstration.');
+        if( isSentOtp ) response.send(JSON.stringify({ "Message": "Existing user. OTP has been sent to your registered mobile number..." }));
+        else response.status( 500 ).send(JSON.stringify({ "Error": "Server is down Please contact your IT adminstration." }));
         return;
     }
     console.log(false);
-    response.status(404).send("Oops! Something went wrong..... User not created.....!!");
+    response.status(404).send(JSON.stringify({ "Error": "Oops! Something went wrong..... User not created.....!!" }));
 });
 
 
-
-router.put('/:user_id' , async ( request , response ) => {
+router.put('/:user_id', async ( request , response ) => {
 
     const { body } = request;
   
     if (!validate(body, response)) return;
 
     const updateUser = await users.updateUser(body, request.params.user_id);
+
+    response.setHeader('Content-Type', 'application/json');
+
     if (updateUser){
 
         console.log(true);
-        response.send(updateUser);
+        // response.send(updateUser);
+        response.send(JSON.stringify({ "Message": updateUser }));
         return
 
     }
     console.log(false);
-    response.status(404).send("Oops! Something went wrong..... User not updated.....!!");
+    response.status(404).send(JSON.stringify({ "Error": "Oops! Something went wrong..... User not updated.....!!"}));
+});
+
+
+router.patch('/block/:user_id', async(request, response) => {
+
+    const { body } = request;
+
+    const isBlocked = await users.changeStatus(request.params.user_id, body.userStatus);
+
+    response.setHeader('Content-Type', 'application/json');
+
+    if (isBlocked){
+
+        console.log(true);
+        response.send(JSON.stringify({ "Message": "The selected user blocked successfully......!!"}));
+        return
+    }
+    console.log(false);
+    response.status(404).send(JSON.stringify({ "Error": "Oops! Something went wrong..... OR Seems user does not exists.....!!"}));
+});
+
+
+router.patch('/role/:user_id', async(request, response) => {
+
+    const { body } = request;
+
+    const isRoleChanged = await users.changeRole(body.role_id, request.params.user_id);
+
+    response.setHeader('Content-Type', 'application/json');
+
+    if (isRoleChanged){
+
+        console.log(true);
+        response.send(JSON.stringify({ "Message": "The role changed for selected user successfully......!!"}));
+        return
+    }
+    console.log(false);
+    response.status(404).send(JSON.stringify({ "Error": "Oops! Something went wrong..... Role not changed.....!!"}));
 });
 
 
 router.delete('/:user_id', async ( request , response )=>{
 
-    const removeUser = await users.deleteUser(request.params.user_id);
-    if (removeUser){
+    const deleteUser = await users.deleteUser(request.params.user_id);
+
+    response.setHeader('Content-Type', 'application/json');
+
+    if (deleteUser){
 
         console.log(true);
-        response.send("The selected user deleted successfully......!!");
+        response.send(JSON.stringify({ "Message": "The selected user deleted successfully......!!"}));
         return
-
     }
     console.log(false);
-    response.status(404).send("Oops! Something went wrong..... user not deleted.....!!");
+    response.status(404).send(JSON.stringify({ "Error": "Oops! Something went wrong..... user not deleted.....!!"}));
 });
 
 
