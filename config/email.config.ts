dc591e4f701fc766b0648ed16c2b3593bb465c6a
Nitin Singh <nitin.singh@edgeaxis.in>
2022-11-25 10:10:07 +0530

    Added Comment

--- config/email.config.ts
+++ config/email.config.ts
@@ -1,13 +1,28 @@
 
+/* Email Configuration  */
+
+
+
+
+
+
 export const configuration = {
-    host:'smtp.ethereal.com',
-    port: 587 , 
-    secure : false ,
+
+    host:'smtp.ethereal.com',  // host server 
+    port: 587 ,  // port 
+    secure : false , // secure
+
+
+    // Authrization Details
     auth: {
         user: 'domenic.collins9@ethereal.email',
         pass: 'EFebtqACaxsyGMwxkn'
     }
+
 }
 
+
+
+/* Sending from the account for e.g. From : Nitin Singh <example@domail.com> */
 export const emailHost:string = 'Nitin Singh" <domenic.collins9@ethereal.email>'
 
